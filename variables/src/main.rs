3375b2af336a60f4b351b2583b0a2851067d12d9
Sam Tyson <samtyson555@gmail.com>
2023-09-11 22:36:19 +0100

    mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    const CANT_CHANGE_ME: u32 = 69420;
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x += 6;
+    println!("{x} is x");
+    println!("Can't change me {CANT_CHANGE_ME}");
+    // CANT_CHANGE_ME += 1;
+}
