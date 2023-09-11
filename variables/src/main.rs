e23b0fb660b3c64bf093bc819d98a3c646469b48
Sam Tyson <samtyson555@gmail.com>
2023-09-11 22:52:43 +0100

    hex n that

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use hex;
+
 fn main() {
     const CANT_CHANGE_ME: u32 = 69420;
     let mut x: i32 = 5;
@@ -11,5 +13,13 @@ fn main() {
     let parsley: u32 = x_string.parse().unwrap();
     println!("{parsley} is a number again ?? ");
     println!("A number of this length: {x_len}");
+
+    let hexy: i32 = 0xff;
+
+    println!("{hexy}");
+
+    let mut password: String = "Hello".to_string();
+    password = hex::encode(password);
+    println!("{password}");
     // CANT_CHANGE_ME += 1;
 }
