e2cc9b1aa7d1154c99e061a9a12c069125cff57d
Sam Tyson <samtyson555@gmail.com>
2023-09-12 20:53:06 +0100

    comments and returns

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     let y = {
         let x = 3;
-        x + 1
+        x + five()
     };
 
 
@@ -17,6 +17,13 @@ fn print_labelled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}")
 }
 
+fn five() -> i32 {
+    5
+}
+
 fn another_function(x: i32) {
     println!("The value of x is {x}");
 }
+// there is 
+// no multi-line comments
+// sad
