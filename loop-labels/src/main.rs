07071afb5ac5c44162a26d71bc163377182e8563
Sam Tyson <samtyson555@gmail.com>
2023-09-16 17:37:26 +0100

    list generation? ? ?

--- loop-labels/src/main.rs
+++ loop-labels/src/main.rs
@@ -8,4 +8,35 @@ fn main() {
             }
         }
     }
+
+    conditional_loops();
+
+    for_loop();
 }
+
+fn conditional_loops() { 
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+
+    }
+
+    println!("Lift off !!! ");
+}
+
+fn for_loop() {
+    // Rip python
+
+    let a: Vec<i32> = (10..60).step_by(10).rev().collect();
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
